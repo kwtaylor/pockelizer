2003182020c990532cfea29d67009570fbedb933
PenguiNx42 <taylork@alum.mit.edu>
2015-05-13 21:17:21 -0700

    Updated readme, added licenses
    Low level code released under permissive licenses. I'm still deciding
    how to release the upper-level application code.

--- pocketlizer.sdc
+++ pocketlizer.sdc
@@ -1,2 +1,4 @@
+# This file is released in the public domain and may be used, modified and redistributed without restriction
+
 create_clock -add -period 50Mhz -name clk [get_ports clk]
 derive_clock_uncertainty
--- touch_ctrl.v
+++ touch_ctrl.v
@@ -1,4 +1,26 @@
-// talk to FT6206 capacative touch screen via i2c
+//Copyright (c) 2015 Ken Taylor
+// File touch_ctrl.v originally from https://github.com/kwtaylor/pockelizer
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+// Poll the FT6206 capacative touch screen via i2c
+// for the Adafruit ILI9341 Breakout and Shield display
 
 module touch_ctrl #(
     parameter ADDR = 7'h38, // FT6206 i2c address
